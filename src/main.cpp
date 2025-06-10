7d82df3f1fa1cf104c52ed388c1b158d79a30de0
CongDangLuu <congdang143@gmail.com>
2025-06-10 20:19:07 +0700

    read a101 data use uart 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <HardwareSerial.h>
+
+// Define UART parameters
+#define UART1_TX_PIN 17 
+#define UART1_RX_PIN 16
+#define BAUD_RATE_1 115200
+#define BAUD_RATE_2 921600
+
+HardwareSerial a101Serial(1); // Create a HardwareSerial object for UART1
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  Serial.begin(BAUD_RATE_2);
+  a101Serial.begin(BAUD_RATE_1, SERIAL_8N1, UART1_RX_PIN, UART1_TX_PIN);
+
+  // Optional: Send initial commands to the camera
+  a101Serial.print("AT+FPS=19\r");  // Set frames per second
+  delay(100);
+  a101Serial.print("AT+DISP=4\r");  // Set display mode
+  delay(100);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+// Pass data from the camera to the serial output
+  while (a101Serial.available()) {
+    Serial.write(a101Serial.read());  // Forward the camera's data to the serial monitor
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
