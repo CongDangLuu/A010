3c643ff4a4ffe4f1457900f0ba5f7bae1e84ef28
vudinh98 <dinhquang.vu1310@gmail.com>
2025-06-11 22:44:14 +0700

    Update project config & serial of sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <Arduino.h>
+#include <string>
 
 #include <HardwareSerial.h>
 
 // Define UART parameters
 #define UART1_TX_PIN 43 
 #define UART1_RX_PIN 44
+
 #define BAUD_RATE_1 115200
-#define BAUD_RATE_2 921600
+#define BAUD_RATE_2 115200
 
 HardwareSerial a101Serial(1); // Create a HardwareSerial object for UART1
+char buffer[20];
 
 void setup() {
 
@@ -25,7 +28,14 @@ void setup() {
 void loop() {
 // Pass data from the camera to the serial output
   while (a101Serial.available()) {
-    Serial.write(a101Serial.read());  // Forward the camera's data to the serial monitor
+    // Serial.write("a101Serial.read() \r\n");
+    // Serial.write(a101Serial.read());  // Forward the camera's data to the serial monitor
+    int num = a101Serial.read();
+    sprintf(buffer, "%d \r\n", num);
+    Serial.write((char *)buffer);  // Forward the camera's data to the serial monitor
+    delay(1000);
   }
+  Serial.write("while() loop \r\n");
+  delay(1000); // Add a small delay to avoid overwhelming the serial output
 }
 
